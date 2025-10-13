bc6a2f8e38850c8974e97eb1c5d13caa288c9d78
fsegurai <fabiansegu@gmail.com>
2025-10-13 19:48:50 +0000

    [🌎] Clean update: Update GitHub Pages content

--- dist/playground.js
+++ dist/playground.js
@@ -1,4 +1,4 @@
-import { m as mdRender, g as generateTOC, s as setupMobileToggle, a as setupSmoothScroll, i as initScrollspy } from './toc-C5lrwS4L.js';
+import { m as mdRender, g as generateTOC, s as setupMobileToggle, a as setupSmoothScroll, i as initScrollspy } from './toc-CFJeyLJc.js';
 
 const mdSample = `
 # 🎢 ScrollSpy Playground Sample
